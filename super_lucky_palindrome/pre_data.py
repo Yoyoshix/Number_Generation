16e05e15fa9e9152b26eb021e0ec5579b0d279cd
Yoyoshi <tyoshim@gmail.com>
2019-04-18 15:31:14 +0200

    Update pre_data.py

--- super_lucky_palindrome/pre_data.py
+++ super_lucky_palindrome/pre_data.py
@@ -5,18 +5,20 @@ def is_lucky(length):
         return 0
     return 1
 
-def possibilities(length):
+def get_pair(length):
     pair = []
     limit = length//2    #palindrom middle
     is_odd = length%2
-    total = 0
     for x in range(0, limit+1):
         #Need to *2 because we will process only one side of palindrome
         if is_lucky(x*2+is_odd) == 1 or is_lucky(x*2) == 1 or is_lucky(length-x*2-is_odd) == 1 or is_lucky(length-x*2) == 1:
             pair.append(x)
+    return pair
+
+def get_possibilities(length, pair):
+    total = 0
     for i in pair:
-        total += scipy.special.binom(limit, i)
-    print(length, pair, int(total))
+        total += scipy.special.comb(length, i, exact=True)
     return total
 
 for i in range(4,4445): #We hit the infinity barrier at 4444 number length 
