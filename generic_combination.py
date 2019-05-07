01950e9086b113642528f4049993ad289c437e5a
Yoyoshi <tyoshim@gmail.com>
2019-05-07 11:39:48 +0200

    Update generic_combination.py
    Still need to add context of algorithm at beginning

--- generic_combination.py
+++ generic_combination.py
@@ -1,49 +1,68 @@
+import copy
 import scipy.special
 
-def get_comb(length, pair, offset=0):
-    comb = []
-    for pos in range(1, length+1):
-        total = 0
-        for i in pair:
-            i -= offset
-            if i <= pos:
-                total += scipy.special.comb(pos, i, exact=True)
-        comb.append(total)
-    return comb
-
-def solve(length, pair, cursor):
-    if cursor >= get_comb(length, pair)[-1] or cursor < 0:
-        return None
-    
-    res = ["0"] * length
-    pos = 0
-    amount = 0
-    
-    while cursor > 0:
-        comb = get_comb(length, pair, amount)
-        idx = 0
-        while idx < len(comb) and cursor >= comb[idx]:
-            idx += 1
-        res[idx] = "1"
-        
-        if idx > 0:
-            cursor -= comb[idx-1]
+def get_possibilities(length, pair): #determine how much possibility for a given length
+    total = 0
+    for i in pair:
+        if i <= length:
+            total += scipy.special.comb(length, i, exact=True) #exact=True return a int instead of a float
         else:
             break
-        length = idx
-        amount += 1
-    
-    if cursor == 0:
-        idx = 0
-        while pair[idx] < amount:
-            idx += 1
-        for i in range(pair[idx] - amount):
-            res[i] = "1"
-    
-    return "".join(res[::-1])
+    return total
 
+def exponential_search(length, pair, query):
+    idx = length-2 #start at end of array
+    step = 1 #helps moving on the array 
+    current = -1 #store current value
+    best_idx = -1 #return idx
+    best_val = 0 #return value
+    over_it = False #once we've got over the target we will change the behavior of step
 
-length = 4
-pair = [2,3,4]
-for cursor in range(get_comb(length, pair)[-1]):
-    print(solve(length, pair, cursor), cursor)
+    while step > 0: #while we can move
+        if over_it == True:
+            step = step // 2 #need to reduce the steps to get precise
+
+        current = get_possibilities(idx+1, pair)
+        if current <= query:
+            if over_it == False: #exception when breaking the point for the first time
+                step = step // 4
+                over_it = True #we know we crossed the line so we now need to get precise
+                if idx == length-2: #another exception
+                    step = 1
+            best_val = current #saves the best values
+            best_idx = idx
+            idx += step
+        else:
+            idx -= step
+
+        if over_it == False:
+            step *= 2       #need to increase the step to go further and faster
+
+    return best_idx+1, best_val
+
+def generate(length, pair_numbers, query, digits=["0", "1"]):
+    space = length
+    pair = copy.copy(pair_numbers)
+    solution = [digits[0]] * length
+    
+    idx = 0
+    remove = 1
+    while query > 0 and remove > 0:
+        idx, remove = exponential_search(space, pair, query)
+        if idx >= length:
+            return None
+        solution[idx] = digits[1]
+
+        query -= remove
+        space = idx
+        for i in range(len(pair)): #update pair numbers
+            pair[i] -= 1
+        if pair[0] < 0: #delete because useless
+            del pair[0]
+
+    if query == 0:
+        for i in range(pair[0]): #sometimes there is some remaining digits to put
+            solution[i] = digits[1]
+    space = 0
+
+    return "".join(solution[::-1])
